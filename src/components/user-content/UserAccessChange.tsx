f18cf9776e602bb272a20e88bea28515d0a910bd
NursultanFront <nursultan020395@gmail.com>
2023-07-02 15:59:32 +0500

    fix: styles

--- src/components/user-content/UserAccessChange.tsx
+++ src/components/user-content/UserAccessChange.tsx
@@ -22,7 +22,7 @@ const UserAccessChange = ({ closeAdvance }: Props) => {
   return (
     <>
       <Dropdown icon={false}>
-        <Dropdown.Menu open className="menu-reset">
+        <Dropdown.Menu open className="menu-reset access-menu">
           <form className="user-dropdown">
             {options.map((item) => {
               return (
